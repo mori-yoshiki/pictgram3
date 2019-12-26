fd877b31d3f639e1a6751c5b57c432892a168458
”mori” <moriyoshiki_seed@yahoo.co.jp>
2019-12-26 12:40:15 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15}
+  
+  VALID_EMAIL_REGEX = /[^\s]@[^\s]/
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
+  validates :password, presence: true, length: { minimum:8, maximum: 32},
+  format: {with: VALID_PASSWORD_REGEX}
+
+  
+
+
+
+  has_secure_password
+  
+  
+  
+end
